b4f950240f9625f193799a34f0207b0ee9408186
Renganathan.M <renganathan.m@gmail.com>
2022-10-31 20:44:29 +0000

    Next commit.

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('This is for git practicing.')
+    print_hi("Ready for another commit")
 
