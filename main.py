658b48fa4355c5b3de099854ea5e0270a5c734ef
Renganathan.M <renganathan.m@gmail.com>
2022-10-31 20:59:32 +0000

    Adding new feature.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def features():
+    print("New feature is added")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
